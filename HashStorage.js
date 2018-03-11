b7cea7aada4cce371669798cdb9cacb741743596
Skazka123 <olgayakimovich87@gmail.com>
2018-03-11 13:53:30 +0300

    made changes

--- HashStorage.js
+++ HashStorage.js
@@ -2,28 +2,34 @@
 
 class HashStorage {
     constructor(key, value) {
-        this.key = key;
-        this.value = value;
+        // this.key = key;
+        // this.value = value;
+        this.hash={};
     }
 
     addValue(key, value) {
-        this.key = key;
-        this.value = value;
-        HashStorage.key = value;
+        this.hash[key] = value;
     }
 
     getValue(key) {
-        return this.value;
+        return this.hash[key];
     }
 
     deleteValue(key){
-        if (delete this.key) {
+        if (delete this.hash[key]) {
             return true;
         }
         return false;
     }
 
     getKeys() {
-        return this.key;
+        return Object.keys(this.hash);
     }
 }
+// let drink = new HashStorage;
+// drink.addValue('name', 'Margarita');
+// console.log(drink);
+// console.log(drink.getKeys());
+// console.log(drink.getValue('name'));
+// console.log(drink.deleteValue('name'));
+// console.log(drink);
